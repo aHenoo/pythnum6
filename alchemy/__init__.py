caba26f758336758fcce2269ca91f011a74e51ec
Axel Heno <aheno@k2r3p9.42mulhouse.fr>
2026-03-19 10:35:31 +0100

    fini

--- alchemy/__init__.py
+++ alchemy/__init__.py
@@ -3,4 +3,4 @@ __author__ = "Master Pythonicus"
 
 from .elements import create_fire, create_water
 
-__all__ = ["__version__", "__author__", "create_fire", "create_water"]
+__all__ = ["create_fire", "create_water"]
